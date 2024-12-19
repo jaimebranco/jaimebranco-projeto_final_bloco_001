70abf4f7b1b82148c2a9c779efbc12553bfd5851
unknown <jaimebrancofilho@gmail.com>
2024-12-19 10:39:34 -0300

    Primeira etapa - criando o menu principal -

---
+++ src/projeto_final_bloco_01/MenuPrincipal.java
@@ -0,0 +1,78 @@
+package projeto_final_bloco_01;
+
+import java.util.Scanner;
+
+import conta.util.Cores;
+
+public class MenuPrincipal {
+
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+		int opcao,  ano; 
+		String nome, editora, autor;
+		
+		while (true) {
+			System.out.println("                                                     ");
+			System.out.println("               SERVIÇO DE EMPRÉSTIMOS                ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("            1 - Cadastrar livro                      ");
+			System.out.println("            2 - Listar livros                        ");
+			System.out.println("            3 - Consultar livro                      ");
+			System.out.println("            4 - Empréstimo                           ");
+			System.out.println("            5 - Devolução                            ");
+			System.out.println("            6 - Sair                                 ");
+			System.out.println("                         				             ");
+			System.out.println("*****************************************************");
+			System.out.println("Entre com a opção desejada:                          ");
+			System.out.println("                                                     ");
+
+			opcao = leia.nextInt();
+
+			switch (opcao) {
+			
+			case 1:
+				System.out.println("             CADASTRO DE LIVROS                  ");
+				System.out.println("                         				         ");
+				System.out.println("Digite o nome do livro");
+				leia.skip("\\R?");
+				nome = leia.nextLine();
+				
+				System.out.println("Digite o autor do livro - " + nome + " : ");
+				autor = leia.nextLine();
+				System.out.println("Digite a editora:");
+				editora = leia.nextLine();
+				System.out.println("Digite o ano de publicação:");
+				ano = leia.nextInt();
+				
+				System.out.println("Livro Adicionado com sucesso");
+				
+				break;
+			case 2:
+				System.out.println("            LISTA DE TODOS OS LIVROS             ");
+				
+				break;
+			case 3:
+				System.out.println("               CONSULTA DE LIVROS                ");
+				
+				break;
+			case 4:
+				System.out.println("              EMPRÉSTIMO DE LIVROS               ");
+				
+				break;
+			case 5: 
+				System.out.println("                  DEVOLUÇÃO                      ");
+				
+				
+				break;		
+			default:
+				System.out.println("             *** opção inválida ***              ");
+				System.out.println("                 Digite novamente                ");
+				break;
+			}
+
+		}
+
+	}
+}
